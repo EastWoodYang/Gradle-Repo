fec8326d4f5dafe91e91171f74b45fee75c52ae6
yangchengdong <yangchengdong@yingyinglicai.com>
2018-12-28 16:08:34 +0800

    1. clean useless code. 2. increase git compatibility.

--- gradle-repo/src/main/groovy/com/eastwood/tools/plugins/repo/GitUtil.groovy
+++ gradle-repo/src/main/groovy/com/eastwood/tools/plugins/repo/GitUtil.groovy
@@ -3,18 +3,18 @@ package com.eastwood.tools.plugins.repo
 class GitUtil {
 
     static void clone(File dir, String url, String branchName) {
-        def process = ("git clone -b " + branchName + " " + url + " -l " + dir.name).execute(null, dir.parentFile)
+        def process = ("git clone -b $branchName $url -l $dir.name").execute(null, dir.parentFile)
         def result = process.waitFor()
         if (result != 0) {
-            throw new RuntimeException("[repo] - fail to execute git command [git clone -b $branchName $url \".\"] under ${dir.absolutePath}\n message: ${process.err.text}")
+            throw new RuntimeException("[repo] - failure to execute git command [git clone -b $branchName $url -l $dir.name] under ${dir.absolutePath}\n message: ${process.err.text}")
         }
     }
 
     static void commit(File dir, String message) {
-        def process = ("git commit -m \"" + message + "\"").execute(null, dir)
+        def process = ("git commit -m \"$message\"").execute(null, dir)
         def result = process.waitFor()
         if (result != 0) {
-            throw new RuntimeException("[repo] - fail to execute git command [git commit -m \"" + message + "\"] under ${dir.absolutePath}\n message: ${process.err.text}")
+            throw new RuntimeException("[repo] - failure to execute git command [git commit -m \"$message\"] under ${dir.absolutePath}\n message: ${process.err.text}")
         }
     }
 
@@ -26,7 +26,7 @@ class GitUtil {
         def process = ("git remote -v").execute(null, dir)
         def result = process.waitFor()
         if (result != 0) {
-            throw new RuntimeException("[repo] - fail to execute git command [git remote -v] under ${dir.absolutePath}\n message: ${process.err.text}")
+            throw new RuntimeException("[repo] - failure to execute git command [git remote -v] under ${dir.absolutePath}\n message: ${process.err.text}")
         }
 
         def url = null
@@ -36,18 +36,28 @@ class GitUtil {
             }
         }
         if (url == null) {
-            throw new RuntimeException("[repo] - fail to get origin remote fetch fetchUrl.")
+            throw new RuntimeException("[repo] - failure to get origin remote fetch fetchUrl.")
         }
         return url
     }
 
     static String getBranchName(File dir) {
-        def process = ("git symbolic-ref --short -q HEAD").execute(null, dir)
+        def process = ("git branch").execute(null, dir)
         def result = process.waitFor()
         if (result != 0) {
-            throw new RuntimeException("[repo] - fail to execute git command [git symbolic-ref --short -q HEAD] under ${dir.absolutePath}\n message: ${process.err.text}")
+            throw new RuntimeException("[repo] - failure to execute git command [git branch] under ${dir.absolutePath}\n message: ${process.err.text}")
         }
-        return process.text.trim()
+        String branchName = null
+        process.text.readLines().each {
+            if (it.startsWith('*')) {
+                branchName = it.replace('*', '').trim()
+            }
+        }
+
+        if (branchName == null) {
+            throw new RuntimeException("[repo] - failure to get git branch name under ${dir.absolutePath}")
+        }
+        return branchName
     }
 
     static boolean isBranchChanged(File dir, String branch) {
@@ -59,7 +69,7 @@ class GitUtil {
         def process = ("git status -s").execute(null, dir)
         def result = process.waitFor()
         if (result != 0) {
-            throw new RuntimeException("[repo] - git fail to execute [git status -s] under ${dir.absolutePath}\n message: ${process.err.text}")
+            throw new RuntimeException("[repo] - failure to execute git command [git status -s] under ${dir.absolutePath}\n message: ${process.err.text}")
         }
         return process.text.trim() == ""
     }
@@ -72,7 +82,7 @@ class GitUtil {
         def process = ("git fetch").execute(null, dir)
         def result = process.waitFor()
         if (result != 0) {
-            throw new RuntimeException("[repo] - git fail to execute [git fetch] under ${dir.absolutePath}\n message: ${process.err.text}")
+            throw new RuntimeException("[repo] - failure to execute git command [git fetch] under ${dir.absolutePath}\n message: ${process.err.text}")
         }
         if (branchName == "master") {
             branchName = "HEAD"
@@ -81,10 +91,10 @@ class GitUtil {
     }
 
     static void checkoutBranch(File dir, String branchName) {
-        def process = ("git checkout " + branchName).execute(null, dir)
+        def process = ("git checkout $branchName").execute(null, dir)
         def result = process.waitFor()
         if (result != 0) {
-            throw new RuntimeException("[repo] - git fail to execute [git checkout $branchName] under ${dir.absolutePath}\n message: ${process.err.text}")
+            throw new RuntimeException("[repo] - failure to execute git command [git checkout $branchName] under ${dir.absolutePath}\n message: ${process.err.text}")
         }
     }
 
@@ -92,15 +102,15 @@ class GitUtil {
         def process = ("git checkout -b $branchName origin/$branchName").execute(null, dir)
         def result = process.waitFor()
         if (result != 0) {
-            throw new RuntimeException("[repo] - git fail to execute [git checkout -b $branchName origin/$branchName] under ${dir.absolutePath}\n message: ${process.err.text}")
+            throw new RuntimeException("[repo] - failure to execute git command [git checkout -b $branchName origin/$branchName] under ${dir.absolutePath}\n message: ${process.err.text}")
         }
     }
 
     static void checkoutNewBranch(File dir, String branchName) {
-        def process = ("git checkout -b " + branchName).execute(null, dir)
+        def process = ("git checkout -b $branchName").execute(null, dir)
         def result = process.waitFor()
         if (result != 0) {
-            throw new RuntimeException("[repo] - git fail to execute [git checkout -b $branchName] under ${dir.absolutePath}\n message: ${process.err.text}")
+            throw new RuntimeException("[repo] - failure to execute git command [git checkout -b $branchName] under ${dir.absolutePath}\n message: ${process.err.text}")
         }
     }
 
@@ -108,7 +118,7 @@ class GitUtil {
         def process = ("git add repo.xml").execute(null, dir)
         def result = process.waitFor()
         if (result != 0) {
-            throw new RuntimeException("[repo] - git fail to execute [git add repo.xml] under ${dir.absolutePath}\n message: ${process.err.text}")
+            throw new RuntimeException("[repo] - failure to execute git command [git add repo.xml] under ${dir.absolutePath}\n message: ${process.err.text}")
         }
 
         commit(dir, "keep repo.xml")
@@ -118,7 +128,7 @@ class GitUtil {
         def process = ("git checkout repo.xml").execute(null, dir)
         def result = process.waitFor()
         if (result != 0) {
-            throw new RuntimeException("[repo] - git fail to execute [git checkout repo.xml] under ${dir.absolutePath}\n message: ${process.err.text}")
+            throw new RuntimeException("[repo] - failure to execute git command [git checkout repo.xml] under ${dir.absolutePath}\n message: ${process.err.text}")
         }
     }
 
--- gradle-repo/src/main/groovy/com/eastwood/tools/plugins/repo/RepoSettingsPlugin.groovy
+++ gradle-repo/src/main/groovy/com/eastwood/tools/plugins/repo/RepoSettingsPlugin.groovy
@@ -16,11 +16,12 @@ class RepoSettingsPlugin implements Plugin<Settings> {
         projectDir = settings.rootProject.projectDir
 
         RepoInfo repoInfo = RepoUtil.getRepoInfo(projectDir, false)
+
         boolean initialized = GitUtil.isGitDir(projectDir)
         if (initialized) {
             String fetchUrl = GitUtil.getOriginRemoteFetchUrl(projectDir)
             if (repoInfo.projectInfo == null || repoInfo.projectInfo.repositoryInfo == null || fetchUrl != repoInfo.projectInfo.repositoryInfo.fetchUrl) {
-                throw new RuntimeException("[repo] - project git remote origin fetch url is changed.")
+                throw new RuntimeException("[repo] - project '${settings.rootProject.getName()}': git remote origin fetch url is changed.")
             }
 
             RepositoryInfo projectRepositoryInfo = repoInfo.projectInfo.repositoryInfo
@@ -29,16 +30,16 @@ class RepoSettingsPlugin implements Plugin<Settings> {
                 if (GitUtil.isLocalBranch(projectDir, projectRepositoryInfo.branch)) {
                     GitUtil.revertRepoFile(projectDir)
                     GitUtil.checkoutBranch(projectDir, projectRepositoryInfo.branch)
-                    println "[repo] - git checkout $projectRepositoryInfo.branch"
+                    println "[repo] - project '${settings.rootProject.getName()}': git checkout $projectRepositoryInfo.branch"
                 } else {
                     if (GitUtil.isRemoteBranch(projectDir, projectRepositoryInfo.branch)) {
                         GitUtil.revertRepoFile(projectDir)
                         GitUtil.checkoutRemoteBranch(projectDir, projectRepositoryInfo.branch)
-                        println "[repo] - git checkout -b $projectRepositoryInfo.branch origin/$projectRepositoryInfo.branch"
+                        println "[repo] - project '${settings.rootProject.getName()}': git checkout -b $projectRepositoryInfo.branch origin/$projectRepositoryInfo.branch"
                     } else {
                         GitUtil.checkoutNewBranch(projectDir, projectRepositoryInfo.branch)
                         GitUtil.commitRepoFile(projectDir)
-                        println "[repo] - git checkout -b $projectRepositoryInfo.branch"
+                        println "[repo] - project '${settings.rootProject.getName()}': git checkout -b $projectRepositoryInfo.branch"
                     }
                 }
                 repoInfo = RepoUtil.getRepoInfo(projectDir, false)
@@ -63,7 +64,7 @@ class RepoSettingsPlugin implements Plugin<Settings> {
                 if (moduleInitialized) {
                     String remoteUrl = GitUtil.getOriginRemoteFetchUrl(moduleDir)
                     if (moduleInfo.repositoryInfo == null || remoteUrl != moduleInfo.repositoryInfo.fetchUrl) {
-                        throw new RuntimeException("[repo] - module [$moduleName] git remote origin fetch url is changed.")
+                        throw new RuntimeException("[repo] - module '$moduleName': git remote origin fetch url is changed.")
                     }
 
                     String branch = moduleInfo.repositoryInfo.branch
@@ -73,18 +74,18 @@ class RepoSettingsPlugin implements Plugin<Settings> {
                         if (isClean) {
                             if (GitUtil.isLocalBranch(moduleDir, branch)) {
                                 GitUtil.checkoutBranch(moduleDir, branch)
-                                println "[repo] - git checkout $branch"
+                                println "[repo] - module '$moduleName': git checkout $branch"
                             } else {
                                 if (GitUtil.isRemoteBranch(moduleDir, branch)) {
                                     GitUtil.checkoutRemoteBranch(moduleDir, branch)
-                                    println "[repo] - git checkout -b $branch origin/$branch"
+                                    println "[repo] - module '$moduleName': git checkout -b $branch origin/$branch"
                                 } else {
                                     GitUtil.checkoutNewBranch(moduleDir, branch)
-                                    println "[repo] - git checkout -b $branch"
+                                    println "[repo] - module '$moduleName': git checkout -b $branch"
                                 }
                             }
                         } else {
-                            throw new RuntimeException("[repo] - module [$moduleName] changes not staged for commit.")
+                            throw new RuntimeException("[repo] - module '$moduleName': please commit or revert changes before checkout other branch.")
                         }
                     }
                 } else {
@@ -95,7 +96,7 @@ class RepoSettingsPlugin implements Plugin<Settings> {
                     if (repositoryInfo == null) return
 
                     String originUrl = repositoryInfo.fetchUrl
-                    println "[repo] - git clone $originUrl -b $repositoryInfo.branch"
+                    println "[repo] - module '$moduleName': git clone $originUrl -b $repositoryInfo.branch"
                     GitUtil.clone(moduleDir, originUrl, repositoryInfo.branch)
                 }
             } else {
@@ -104,7 +105,7 @@ class RepoSettingsPlugin implements Plugin<Settings> {
                 if (repositoryInfo == null) return
 
                 String originUrl = repositoryInfo.fetchUrl
-                println "[repo] - git clone $originUrl -b $repositoryInfo.branch"
+                println "[repo] - module '$moduleName': git clone $originUrl -b $repositoryInfo.branch"
                 GitUtil.clone(moduleDir, originUrl, repositoryInfo.branch)
             }
         }
@@ -122,14 +123,14 @@ class RepoSettingsPlugin implements Plugin<Settings> {
         def process = ("git status -s").execute(null, projectDir)
         def result = process.waitFor()
         if (result != 0) {
-            throw new RuntimeException("[repo] - fail to execute git command [git status -s] under ${projectDir.absolutePath}\n message: ${process.err.text}")
+            throw new RuntimeException("[repo] - failure to execute git command [git status -s] under ${projectDir.absolutePath}\n message: ${process.err.text}")
         }
         def info = process.text.readLines()
         int changeSize = info.size()
         boolean isClean = changeSize == 1 && info.get(0).endsWith('repo.xml')
         if (!isClean) {
             GitUtil.revertRepoFile(projectDir)
-            throw new RuntimeException("[repo] - ${settings.rootProject.getName()}: changes not staged for commit.")
+            throw new RuntimeException("[repo] - project '${settings.rootProject.getName()}': please commit or revert changes before checkout other branch.")
         }
 
         // check modules
@@ -142,7 +143,7 @@ class RepoSettingsPlugin implements Plugin<Settings> {
             isClean = GitUtil.isClean(moduleDir)
             if (!isClean) {
                 GitUtil.revertRepoFile(projectDir)
-                throw new RuntimeException("[repo] - ${RepoUtil.getModuleName(projectDir, moduleDir)}: changes not staged for commit.")
+                throw new RuntimeException("[repo] - module '${RepoUtil.getModuleName(projectDir, moduleDir)}': please commit or revert changes before checkout other branch.")
             }
         }
     }
